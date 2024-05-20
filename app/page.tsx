231d2ed51e06797831c9eb082680c9c8ea3f3fb6
martinromario55 <martinromario55@gmail.com>
2024-05-20 15:01:36 +0300

    Element wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+'use client'
 import AdminInfo from '@/components/AdminInfo'
 import Counter from '@/components/Counter'
+import ElementWrapper from '@/components/ElementWrapper'
 import Form from '@/components/Form'
 import User from '@/components/User'
 import UserInfo from '@/components/UserInfo'
@@ -7,7 +9,25 @@ import UserInfo from '@/components/UserInfo'
 const Home = () => {
   return (
     <section>
-      <Form />
+      <ElementWrapper elementType="div" className="border">
+        <p>This is a div element with a class name of border</p>
+      </ElementWrapper>
+
+      <ElementWrapper
+        elementType="h1"
+        className="border text-2xl"
+        onClick={() => alert('h1 clicked')}
+      >
+        Header Text
+      </ElementWrapper>
+
+      <ElementWrapper
+        elementType="button"
+        className="bg-slate-400 p-4 text-2xl"
+        onClick={() => alert('button clicked')}
+      >
+        Click me
+      </ElementWrapper>
     </section>
   )
 }
